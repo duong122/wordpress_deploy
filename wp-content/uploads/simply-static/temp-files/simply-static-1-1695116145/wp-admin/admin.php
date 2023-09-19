3c411b3fb930f3b51d03c8d4202d9d72d6c54712
admin <ledangduong18122003@gmail.com>
2023-09-19 18:40:02 +0700

    first commit

---
+++ wp-content/uploads/simply-static/temp-files/simply-static-1-1695116145/wp-admin/admin.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Redirecting...</title>
+		<meta http-equiv="refresh" content="0;url=https://https://github.com/duong122/wordpress_deploy/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2FTrangBao2%2Fwp-admin%2Fadmin.php%3Fsimply_static_page%3D633&reauth=1">
+	</head>
+	<body>
+		<script type="text/javascript">
+			window.location = "https://https://github.com/duong122/wordpress_deploy/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2FTrangBao2%2Fwp-admin%2Fadmin.php%3Fsimply_static_page%3D633&reauth=1";
+		</script>
+
+		<p>You are being redirected to <a href="https://https://github.com/duong122/wordpress_deploy/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2FTrangBao2%2Fwp-admin%2Fadmin.php%3Fsimply_static_page%3D633&reauth=1">https://https://github.com/duong122/wordpress_deploy/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2FTrangBao2%2Fwp-admin%2Fadmin.php%3Fsimply_static_page%3D633&reauth=1</a></p>
+	</body>
+</html>
